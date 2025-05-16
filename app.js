ac07982b05fec88b7e0f9dc12e27542ede1f957d
Fabio-Moschella <fabiomoschella1999@gmail.com>
2025-05-16 16:03:53 +0200

    add-errorHandler-at-appjs

--- app.js
+++ app.js
@@ -2,12 +2,16 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const postRouter = require("./routers/posts.js");
+const errorHandler = require("./middleware/errorhandler.js");
 
 //STATIC ASSEST
 app.use(express.static("pubblic"));
 app.use(express.json());
 //ROUTERS
 app.use("/post", postRouter);
+
+//MIDDLEWARE
+app.use(errorHandler);
 
 // IL SERVER E IN ASCOLTO SULLA PORTA 3000
 app.listen(port, () => {
