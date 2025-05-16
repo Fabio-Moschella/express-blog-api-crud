b808d625ce371dfaa3dc4f5ad83b1f1d53bf313b
Fabio-Moschella <fabiomoschella1999@gmail.com>
2025-05-16 16:15:10 +0200

    add-errorNotFoundjs

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const port = 3000;
 const postRouter = require("./routers/posts.js");
 const errorHandler = require("./middleware/errorhandler.js");
 
+const errorNotFound = require("./middleware/errorNotFound.js");
+
 //STATIC ASSEST
 app.use(express.static("pubblic"));
 app.use(express.json());
@@ -12,7 +14,7 @@ app.use("/post", postRouter);
 
 //MIDDLEWARE
 app.use(errorHandler);
-
+app.use(errorNotFound);
 // IL SERVER E IN ASCOLTO SULLA PORTA 3000
 app.listen(port, () => {
   console.log("il server è in ascolto sulla porta " + port);
